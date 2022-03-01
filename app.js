33f4f51629133be856a29d4949b0f162f318a10c
Aditya Prabhat <adityaprabhat907@gmail.com>
2022-03-01 11:20:50 +0530

    cookie issues

--- app.js
+++ app.js
@@ -27,9 +27,13 @@ app.use(function(req, res, next) {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   )
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  console.log(req.headers.origin)
   next()
+  
 })
 app.use(cookieParser());
+
 
 app.use(
   cors({
